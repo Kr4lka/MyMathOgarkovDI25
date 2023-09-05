28bf7123e7a9a697865302e9faed8d53638ce7dc
valerchikKirieshka <ythhhd228@gmail.com>
2023-09-05 15:05:10 +0300

    Hello

---
+++ src/main/java/org/example/MyMath.java
@@ -0,0 +1,79 @@
+package org.example;
+
+public class MyMath {
+
+    // Задача №1
+    public static final double PI = 3.1415926535;
+    public static final double E = 2.7182818284;
+
+    public static int maxOfNumbers (int num1, int num2){
+        int maxNum = 0;
+        if (num1 > num2){
+            maxNum = num1;
+        } else {
+            maxNum = num2;
+        }
+        return maxNum;
+    }
+
+    public static double maxOfNumbers (double num1, double num2){
+        double maxNum = 0;
+        if (num1 > num2){
+            maxNum = num1;
+        } else {
+            maxNum = num2;
+        }
+        return maxNum;
+    }
+
+    public static float maxOfNumbers (float num1, float num2){
+        float maxNum = 0;
+        if (num1 > num2){
+            maxNum = num1;
+        } else {
+            maxNum = num2;
+        }
+        return maxNum;
+    }
+
+    public static long maxOfNumbers (long num1, long num2){
+        long maxNum = 0;
+        if (num1 > num2){
+            maxNum = num1;
+        } else {
+            maxNum = num2;
+        }
+        return maxNum;
+    }
+
+    // Задача №2
+    public static double sinus (double sideA, double sideB){
+        double sinAnsw = sideA / sideB;
+        return sinAnsw;
+    }
+
+    public static double cosine (double sideA, double sideC){
+        double cosAnsw = sideA / sideC;
+        return cosAnsw;
+    }
+
+    public static double tangent (double sin, double cos){
+        double tanAnsw = sin / cos;
+        return tanAnsw;
+    }
+
+    public static double squareOfCircle (double radius){
+        double squareAnsw = PI * radius * radius;
+        return squareAnsw;
+    }
+
+    public static double lengthOfCircle (double radius){
+        double lengthAnsw = PI * radius * 2;
+        return lengthAnsw;
+    }
+
+    // Задача №3
+    public static int randomNumber (int minOfRange, int maxOfRange){
+        return 0;
+    }
+}
